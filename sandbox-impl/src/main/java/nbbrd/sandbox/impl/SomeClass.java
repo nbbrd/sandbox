a6b4d5fd77afa152431c7d808de11190e3e4571b
Philippe Charles <philippe.charles@nbb.be>
2022-11-08 15:50:31 +0100

    Some other possibility

--- sandbox-impl/src/main/java/nbbrd/sandbox/impl/SomeClass.java
+++ sandbox-impl/src/main/java/nbbrd/sandbox/impl/SomeClass.java
@@ -1,11 +1,14 @@
 package nbbrd.sandbox.impl;
 
+import lombok.Builder;
+
 @lombok.Value
 @lombok.Builder
 public class SomeClass {
 
     @lombok.NonNull
-    String key;
+    @Builder.Default
+    String key = "123";
 
     public static void main(String[] args) {
         System.out.println(SomeClass.builder().key("abc").build());
