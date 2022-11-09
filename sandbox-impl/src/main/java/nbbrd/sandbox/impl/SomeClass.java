5e863e3780a46d821842be137402659b9d1ab19d
Philippe Charles <philippe.charles@nbb.be>
2022-11-09 09:07:49 +0100

    Add some code

--- sandbox-impl/src/main/java/nbbrd/sandbox/impl/SomeClass.java
+++ sandbox-impl/src/main/java/nbbrd/sandbox/impl/SomeClass.java
@@ -1,6 +1,9 @@
 package nbbrd.sandbox.impl;
 
+import lombok.AccessLevel;
 import lombok.Builder;
+
+import java.util.List;
 
 @lombok.Value
 @lombok.Builder
@@ -10,7 +13,24 @@ public class SomeClass {
     @Builder.Default
     String key = "123";
 
+    @lombok.Singular
+    List<Item> items;
+
     public static void main(String[] args) {
-        System.out.println(SomeClass.builder().key("abc").build());
+        System.out.println(
+                SomeClass
+                        .builder()
+                        .key("abc")
+                        .item(new Item(1, 3))
+                        .item(new Item(3, 1))
+                        .build()
+        );
+    }
+
+    @lombok.AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @lombok.EqualsAndHashCode
+    public static class Item {
+        final int x;
+        final int y;
     }
 }
